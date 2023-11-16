28412c9caa14657212382be2840fc041121ee105
Andrew Rukangu <amr07618@uga.edu>
2023-11-16 11:26:23 -0500

    finished class example with form data and visualizing the data

---
+++ flask_jinja/info2000_package/student_details.py
@@ -0,0 +1,9 @@
+
+# print("in the student details module!!!" + __name__)
+
+class Student:
+    def __init__(self, name = "No name", major = "music", level = 'undergrad', color = 'clear'):
+        self.name = name
+        self.major = major
+        self.level = level
+        self.color = color
